3ee375a99c0980d6cd27b016eb48ddf8266c6b4d
Alexander Verkholomov <Alexander.Verkholomov@sylvamo.com>
2022-04-06 17:08:44 +0300

    0604

--- 02 - LinearRegression/05 - Regression/regression.py
+++ 02 - LinearRegression/05 - Regression/regression.py
@@ -16,6 +16,8 @@ from sklearn.metrics import mean_squared_error as mse
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+
 plt.close('all')
 
 class Regression(object):
@@ -36,11 +38,28 @@ class Regression(object):
         if self.fit_intercept:
             X=np.insert(X,0,1,axis=1)
 
-def linear_regression(X,y):
+def linear_regression(X,y,scaling=True,polynomial=True):
     
     lr=LinearRegression()
     
+    scaler=StandardScaler()
+    
     X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.33)
+    
+    if scaling:
+        
+        scaler.fit(X_train)
+        X_train=scaler.transform(X_train)
+        X_test=scaler.transform(X_test)
+        
+    if polynomial:
+        
+        polynomial=PolynomialFeatures(degree=3,interaction_only=True)
+        X_train=polynomial.fit_transform(X_train)
+        X_test=polynomial.fit_transform(X_test)
+        
+        
+    
     lr.fit(X_train,y_train)
     
     pred_train=lr.predict(X_train)
@@ -68,11 +87,25 @@ def linear_regression(X,y):
     print('Root mean squared erro is {}'.format(np.sqrt(mse(pred_test,y_test))))
     print('Root mean squared erro is {} %'.format(np.sqrt(mse(pred_test,y_test))/np.mean(y_test)*100))
     
-def ridge_regression(X,y,alpha):
+def ridge_regression(X,y,alpha,scaling=True,polynomial=True):
     
     lr=Ridge(alpha=alpha)
     
     X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.33)
+    
+    scaler=StandardScaler()
+    
+    if scaling:
+        
+        X_train=scaler.fit_transform(X_train)
+        X_test=scaler.transform(X_test)
+    
+    if polynomial:
+        
+        polynomial=PolynomialFeatures(degree=3,interaction_only=True)
+        X_train=polynomial.fit_transform(X_train)
+        X_test=polynomial.fit_transform(X_test)
+    
     lr.fit(X_train,y_train)
     
     pred_train=lr.predict(X_train)
@@ -112,15 +145,18 @@ if __name__=="__main__":
        'hasStormProtector', 'basement', 'attic', 'garage', 'hasStorageRoom',
        'hasGuestRoom']]
     
+    
     dat=pd.read_csv('./archive/boston.csv')
     
     y=dat['MEDV']
     X=dat[['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX',
        'PTRATIO', 'B', 'LSTAT']]
     
-    linear_regression(X,y)
     
-    ridge_regression(X,y,alpha=0.1)
+    
+    linear_regression(X,y,scaling=True)
+    
+    ridge_regression(X,y,alpha=0.1,scaling=True)
     
     
     
--- 02 - LinearRegression/05 - Regression/regression_visual.py
+++ 02 - LinearRegression/05 - Regression/regression_visual.py
@@ -11,27 +11,33 @@ import pandas as pd
 
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 from utils import Utils
 
+np.random.seed(102)
 
 X=np.arange(0.01,10,0.1)
 y=np.zeros(len(X))
 
 for i,x in enumerate(X):
     
-    y[i]=(x+x**2+x**3++np.log(x**10)+np.random.normal(0,100,1))
+    y[i]=(x**2+np.random.normal(0,10,1))
     
     
 dat=pd.DataFrame({'x':X,'y':y})
 
 dat['x2']=dat['x']**2
+dat['x3']=dat['x']**3
+dat['x4']=dat['x']**4
+dat['x6']=dat['x']**6
+dat['x8']=dat['x']**8
 
 lr=LinearRegression()
 
+cols=['x','x2','x3','x4','x6']
 
-
-X=dat[['x','x2']]
+X=dat[['x','x2','x3','x4','x6','x8']]
 y=dat['y']
 
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.33,shuffle=True)
@@ -40,10 +46,32 @@ lr.fit(X_train.values,y_train.values.reshape(-1,1))
 
 pred_train=lr.predict(X_train.values)
 pred_test=lr.predict(X_test.values)
+
+#mse[]
+
+pred_train=pd.DataFrame(pred_train,columns=['predicted'])
+pred_train['x']=X_train['x'].values
+
+pred_train=pred_train.sort_values(by='x',ascending=True)
+pred_train.reset_index(inplace=True,drop=True)
+
     
 fig=plt.figure(1)
 ax=fig.add_subplot(111)
-ax.scatter(X,y)
-ax.plot(X_train['x'],pred_train,color=Utils.red_hex,marker='_',lw=4)
-ax.plot(X_test['x'],pred_test,color=Utils.green_hex,marker='o',lw=4)
+ax.scatter(X_train['x'],y_train,color=Utils.blue_hex)
+ax.plot(pred_train['x'].values,pred_train['predicted'],color=Utils.red_hex,lw=4)
+fig.patch.set_facecolor(Utils.black_hex)
+ax.set_facecolor(Utils.black_hex)
+for spine in ax.spines.values():
+    spine.set_edgecolor(Utils.white_hex)
+    spine.set(linewidth=3)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.tick_params(axis='x', colors=Utils.purple_hex,labelsize=15)
+ax.tick_params(axis='y', colors=Utils.purple_hex,labelsize=15)
+ax.grid(True,**Utils.grid_kwargs)
+plt.tight_layout()
+#ax.scatter(X_test['x'],pred_test,color=Utils.green_hex,marker='o',lw=4)
+
+
 
