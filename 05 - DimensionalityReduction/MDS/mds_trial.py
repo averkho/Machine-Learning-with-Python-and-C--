799603e52d0cf9a013b1070635cf07395464e78f
Alexander Verkholomov <Alexander.Verkholomov@sylvamo.com>
2022-06-27 16:09:47 +0300

    2706

--- 05 - DimensionalityReduction/MDS/mds_trial.py
+++ 05 - DimensionalityReduction/MDS/mds_trial.py
@@ -3,6 +3,10 @@ from numpy.random import RandomState
 import matplotlib.pyplot as plt
 from matplotlib import ticker
 from utils import Utils
+import numpy as np
+import pandas as pd
+
+np.random.seed(100)
 
 from sklearn.datasets import make_blobs
 
@@ -17,18 +21,19 @@ from sklearn import manifold, datasets
 
 rng = RandomState(1)
 
+
 n_samples = 1500
 S_points, S_color = datasets.make_s_curve(n_samples, random_state=rng)
 
-def plot_3d(points, points_color, title):
+def plot_3d(i,points, points_color, title):
     x, y, z = points.T
-
-    fig, ax = plt.subplots(
-        tight_layout=True,
-        subplot_kw={"projection": "3d"},
-    )
+    
+    fig = plt.figure(i)
+    
+    ax = fig.add_subplot(111, projection = '3d')
+    
     fig.suptitle(title, size=16)
-    col = ax.scatter(x, y, z, color = Utils.green_hex, s=50, alpha=0.8)
+    col = ax.scatter(x, y, z, c = points_color, s=50, alpha=0.8)
     fig.patch.set_facecolor(Utils.black_hex)
     ax.set_facecolor(Utils.black_hex)
     #ax.view_init(azim=-60, elev=9)
@@ -43,18 +48,18 @@ def plot_3d(points, points_color, title):
     ax.spines['top'].set_visible(False)
     ax.set_title('',color=Utils.purple_hex,
                  fontproperties={'family':Utils.csfont,'size':20})
-    ax.tick_params(axis='x', colors=Utils.purple_hex,labelsize=15)
-    ax.tick_params(axis='y', colors=Utils.purple_hex,labelsize=15)
-    ax.tick_params(axis='z', colors=Utils.purple_hex,labelsize=15)
+    # ax.tick_params(axis='x', colors=Utils.purple_hex,labelsize=15)
+    # ax.tick_params(axis='y', colors=Utils.purple_hex,labelsize=15)
+    # ax.tick_params(axis='z', colors=Utils.purple_hex,labelsize=15)
     plt.show()
 
 
-def plot_2d(points, points_color, title):
+def plot_2d(i,points, points_color, title):
     
-    fig=plt.figure(2, figsize=(3, 3),)
+    fig=plt.figure(i)
     ax = fig.add_subplot(111)
     fig.suptitle(title, size=16)
-    ax.scatter(points[:,0],points[:,1])
+    ax.scatter(points[:,0],points[:,1], c = points_color)
     plt.show()
 
 
@@ -66,17 +71,20 @@ def add_2d_scatter(ax, points, points_color, title=None):
     ax.yaxis.set_major_formatter(ticker.NullFormatter())
 
 
-plot_3d(S_points, S_color,  "Original S-curve samples")
+plot_3d(1,S_points, S_color,  "Original S-curve samples")
 
 n_components = 2
 md_scaling = manifold.MDS(
     n_components=n_components, max_iter=50, n_init=4, random_state=rng
 )
 S_scaling = md_scaling.fit_transform(S_points)
+plot_2d(2,S_scaling, S_color, "Multidimensional scaling")
 
-X,y = make_blobs(100,n_features = 3)
-fig = plt.figure(2)
-ax = fig.add_subplot(111)
-plot_2d(S_scaling, S_color, "Multidimensional scaling")
+X,y = make_blobs(500,n_features = 3)
+S_scaling = md_scaling.fit_transform(X)
+plot_3d(3,X, y,  "Original S-curve samples")
+plot_2d(4,S_scaling, y, "Multidimensional scaling")
 
-A=10
+dat = pd.DataFrame(X,columns = ['x1','x2','x3'])
+dat['y'] = y
+dat.to_csv('./Data/mds_data.csv',index = False)
